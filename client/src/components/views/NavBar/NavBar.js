b781d0e9aaade8f7cf13cdbfaa0195b947bc98ba
hjp2 <nujeeh2@gmail.com>
2021-06-09 01:29:50 +0900

    네비게이션 바 수정
    스크롤바 숨김, 메뉴 순서 변경

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -17,7 +17,7 @@ function NavBar() {
   };
 
   return (
-    <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
+    <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%', overflow:'hidden' }}>
       <div className="menu__logo">
         <a href = "/" style = {{width : "1%", height : "1%", display : "flex", marginLeft : "5px"}}>
           <img src={logo} alt="logo"/>
